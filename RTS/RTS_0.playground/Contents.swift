82d70060d7a8d675f2ca752283543ad920539207
Saki=Hit=Out <goodtreeisagoodshelter@gmail.com>
2015-10-07 13:46:01 +0900

    15/10/07 begin

---
+++ RTS/RTS_0.playground/Contents.swift
@@ -0,0 +1,13 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+let nineBall:Int = 9
+let tenBall:Int = 10
+var invalidBall:Int = 0
+var validBall:Int = 0
+var restBall:Int = 0
+var fallOrMiss:Bool
+restBall = nineBall - invalidBall
+
+if
