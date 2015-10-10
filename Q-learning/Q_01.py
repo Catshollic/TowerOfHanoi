0b4a42716997052179c8fa7ec3fe4efe64a8425e
Saki=Hit=Out <goodtreeisagoodshelter@gmail.com>
2015-10-10 19:37:09 +0900

    first commit

--- Q-learning/Q_01.py
+++ Q-learning/Q_01.py
@@ -1,9 +1,9 @@
-aryNum = 7
+aryNum = 10
 ganma = 0.1
-arufa = 0.5
+arufa =1.0
 r = 0
 learnCount = 10
-ary = [0,0,0,0,0,0,0]
+ary = [0]*10
 
 #print(ary)
 for i in range(learnCount):
@@ -12,9 +12,9 @@ for i in range(learnCount):
 		if i == aryNum-1:
 			r += 1
 			ary[i] = ary[i]+arufa*( r + ganma*0) - ary[i]
-			print(round(ary[i],3)),
-			print(r)
+			print('%d == %0.3f' % (i+1,round(ary[i],3))),
+			print('reward == %d'  % r)
 		else:
 			ary[i] = ary[i]+arufa*( r + ganma*ary[i+1]) - ary[i]
-			print(round(ary[i],3)),
+			print('%d == %0.3f' % (i+1,ary[i]))
 		
